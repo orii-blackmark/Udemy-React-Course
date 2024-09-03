865b92420a7ad656eabf3d6924ce0fe90c10eacf
oriel blackmark <kiplangatoriel@gmail.com>
2024-09-03 21:14:22 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
