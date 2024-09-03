b358c92f8ef22f2a63e095dba7734e733973de56
oriel blackmark <kiplangatoriel@gmail.com>
2024-09-03 22:19:22 +0300

    used React Render Method to render an element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
+const element = (
+
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Phone</th>
+    </tr>
+    <tr>
+      <td>Oriel</td>
+      <td>0758997669</td>
+    </tr>
+  </table>
+)
+
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(
-  <p>Hello World</p>
+  element
 )
