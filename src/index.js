8b0805bff0fb6ec8a42d2c5c3ee7909b0aeae8aa
oriel blackmark <kiplangatoriel@gmail.com>
2024-09-04 16:23:47 +0300

    Modified React Classes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-class Car extends React.Component{
-  render(){
-    return <h2>This is my car </h2>
-  }
+class Car extends React.Component {
+ constructor(props){
+  super(props);
+  this.state = {
+    brand:"BMW",
+    model:"2002",
+    color:"black"
+  };
+ }
+render(){
+  return(
+    <div>
+      <h1>
+        my {this.state.brand}
+      </h1>
+      <p>It is {this.state.color} in color</p>
+      <p>it is a {this.state.model} model</p>
+    </div>
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Car />
 );
