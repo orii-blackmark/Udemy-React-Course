880c5f12e8bc96367f56cd08162104743ded651b
oriel blackmark <kiplangatoriel@gmail.com>
2024-09-04 17:35:53 +0300

    React Events

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Person(props) {
-  return (
-    <h1>
-      My name is {props.name}, and my age is {props.age}
-    </h1>
-  );
-}
-
-function Oriel() {
-  const myobj = { name: "Oriel Kiplangat", age: "24" };
-
-  return (
-    <>
-      <h1>I live in Kenya</h1>
-      <Person name={myobj.name} age={myobj.age} />
-    </>
-  );
+function Person() {
+  const callName = (name) => {
+    alert("Hello " + name);
+  };
+  return <button onClick={callName("Oriel")}>Call Me!</button>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Oriel />);
+root.render(<Person />);
